0cad65de9d57447f6ba9a63d10c757e03a411848
Thundzz <yacinenew@gmail.com>
2014-12-31 15:22:43 +0100

    Fix some stuff

--- Coulaud/TPCraquage/slave.c
+++ Coulaud/TPCraquage/slave.c
@@ -5,15 +5,15 @@
 
 int main()
 {
-	int myrank, result, work;
+	int myrank, recv;
 	MPI_Init( NULL, NULL );
 	MPI_Status status;
 	MPI_Comm Comm_master; 
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 	MPI_Comm_get_parent(&Comm_master);
 
-	MPI_Recv(&result, 1, MPI_INT, 1, MPI_ANY_TAG, Comm_master, &status);
-	MPI_Send(&work, 1 ,MPI_INT, 0, 99, Comm_master);
+	MPI_Recv(&recv, 1, MPI_INT, 0, MPI_ANY_TAG, Comm_master, &status);
+	MPI_Send(&recv, 1 ,MPI_INT, 0, 99, Comm_master);
 
 
 
