45332f7e1007fde5b7df1e06516d7bafeebbbb16
Thundzz <yacinenew@gmail.com>
2014-12-31 17:51:21 +0100

    Avancé le travail

--- Coulaud/TPCraquage/master.c
+++ Coulaud/TPCraquage/master.c
@@ -16,28 +16,14 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	int nb_proc = atoi(argv[1]);
-	int nb_thread = atoi(argv[2]);
+	int nb_proc = MAX(1, atoi(argv[1]));
 	int max_lgth = atoi(argv[4]);
 	char * alphabet = argv[3];
-	char * password = argv[5];
-
 	int alphabet_size = strlen(alphabet);
 
-	int *table = alloc_table(alphabet);
-	char *rev_table = alloc_rev_table(alphabet);
 
-	encoding_table(alphabet, table, rev_table);
-	#ifdef DEBUG
-		int e = encode(table, password, alphabet_size);
-		char d[100];
-		fprintf(stderr, "Encodage  de : %s, %d \n", password, e);
-		decode(rev_table, e, alphabet_size, d);
-	#endif
-
-	int myrank, result, work =42;
+	int myrank;
 	int nb_slaves = nb_proc -1;
-	MPI_Status status; 
 	MPI_Comm slaves;
 	MPI_Init( NULL, NULL );
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
@@ -45,17 +31,11 @@ int main(int argc, char *argv[])
 	MPI_Comm_spawn("slave.out", argv, nb_slaves,
 				 MPI_INFO_NULL, 0, MPI_COMM_SELF, &slaves, MPI_ERRCODES_IGNORE);
 
-	for (int i=0; i < nb_slaves; ++i){
-		work = work +1;
-		MPI_Send(&work, 1 ,MPI_INT, i, 99, slaves);
-	} 
-	for (int i=0; i < nb_slaves; ++i){
-		MPI_Recv(&result, 1, MPI_INT, i, MPI_ANY_TAG, slaves, &status);
-		printf("resultat : %d\n", result);
+	for (int length = 0; length < max_lgth; ++length)
+	{
+
 	}
 
-	free(table);
-	free(rev_table);
 	MPI_Finalize();
 	return 0;
 }
--- Coulaud/TPCraquage/slave.c
+++ Coulaud/TPCraquage/slave.c
@@ -1,22 +1,48 @@
 #include "shared.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <mpi.h>
 
-int main()
+#define EXECNAME "slave.out"
+
+int main(int argc, char* argv[])
 {
-	int myrank, recv;
+	if (argc != 6){
+		fprintf(stderr, "Expecting syntax: "EXECNAME
+			" nb_proc nb_thread alphabet max_lgth password\n");
+		exit(EXIT_FAILURE);
+	}
+	int myrank;
 	MPI_Init( NULL, NULL );
-	MPI_Status status;
 	MPI_Comm Comm_master; 
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 	MPI_Comm_get_parent(&Comm_master);
 
-	MPI_Recv(&recv, 1, MPI_INT, 0, MPI_ANY_TAG, Comm_master, &status);
-	MPI_Send(&recv, 1 ,MPI_INT, 0, 99, Comm_master);
+	int max_lgth = atoi(argv[4]);
+	char * alphabet = argv[3];
+	char * password = argv[5];
+	int alphabet_size = strlen(alphabet);
 
+	int *table = alloc_table(alphabet);
+	char *rev_table = alloc_rev_table(alphabet);
+	encoding_table(alphabet, table, rev_table);
+	char * decoded = malloc(max_lgth* sizeof(char));
 
-
+	int first_task = 0;
+	int last_task = 5;
+	for (int task = first_task; task <= last_task; ++task)
+	{
+		decode(rev_table, task, alphabet_size,  decoded);
+		if(!strcmp(decoded, password))
+		{
+			// Found !!!
+		}
+	}
+	free(decoded);
+	free(table);
+	free(rev_table);
 	MPI_Finalize(); 
 	return 0;
 }
