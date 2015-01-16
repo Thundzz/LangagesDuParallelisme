b3573536c4da9d0620d8c7c4a9d56d401e3c2a24
Nico <nheng@enseirb.fr>
2015-01-16 19:22:08 +0100

    benchmarks2

--- Coulaud/TPCraquage/test.rb
+++ Coulaud/TPCraquage/test.rb
@@ -55,6 +55,45 @@ def launch_bench_procs(max_procs)
 	end
 end
 
+def launch_bench_threads(max_threads)
+	nb_iter = 3
+	d_alphabet="abcdefghijklmnopqrstuvwxyz"
+	d_nbp = 4
+	d_depth = 6
+	password = gen_random_pwd(1+d_depth)
+	time = Array.new(max_threads, 0.0)
+	iter_time = 0.0
+	(2..max_threads).each do |d_nbt|
+		for k in 1..nb_iter
+			options = {}
+			options = {alphabet: d_alphabet, nbp: d_nbp, nbt: d_nbt, depth: d_depth}.merge(options)
+			cmd = "mpiexec -np 1 ./master.out  #{options[:nbp]} "+
+										       "#{options[:nbt]} "+
+										       "#{options[:alphabet]} "+
+										       "#{options[:depth]} "+
+										       "#{password}"
+			puts cmd
+			system(cmd)
+			
+			File.open("time_#{d_nbp}_#{d_nbt}_#{d_depth}.dat", "r") do |file|
+				file.each_line do |line|
+					time[d_nbt] = line.to_s.split(" ")[0].to_f + time[d_nbt].to_f
+				end
+			end
+		end
+	end
+
+	seq = time[2]
+	for i in 2..max_threads
+		time[i] = seq/time[i]
+	end
+	File.open("speedup.dat", "w") do |file|
+		for k in 2..max_threads
+			file.puts("#{k} #{time[k]}")
+		end
+	end
+end
+
 def gen_random_pwd(length)
 	chars = 'abcdefghijklmnopqrstuvwxyz'
 	password = ''
@@ -65,4 +104,4 @@ end
 # try_find_password("zaza")
 # try_find_password("zizou")
 # puts gen_random_pwd 12
-launch_bench_procs 8
+launch_bench_threads 6
